1b595ef7bbbd14dbeee3ee728d12b55080c6e3fd
DanielL492 <144807703+DanielL492@users.noreply.github.com>
2023-12-08 09:17:18 -0500

    xc

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,5 +1,5 @@
 //const URL = `https://www.cheapshark.com/api/1.0/deals?&upperPrice=15`; 
-const URL = `https://www.cheapshark.com/api/1.0/games?title=halo `;
+const URL = `https://www.cheapshark.com/api/1.0/games?title=halo`;
 async function getData(URL) {
   try {
     const response = await fetch(URL);
