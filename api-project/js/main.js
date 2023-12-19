a54ea97a6523405b66f6fe26663b57cfd423187e
DanielL492 <144807703+DanielL492@users.noreply.github.com>
2023-12-19 09:03:02 -0500

    har

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,16 +1,20 @@
 //const URL = `https://www.cheapshark.com/api/1.0/deals?&upperPrice=15`; 
-const URL = `https://www.cheapshark.com/api/1.0/deals?&pageSize=15&upperPrice=15`;
+
+const URL = `https://www.cheapshark.com/api/1.0/random`;
 async function getData(URL) {
   try {
     const response = await fetch(URL);
     const data = await response.json();
     console.log(data);
-    document.querySelector("h3").textContent = data.title;
+    document.querySelector("h3").textContent = data[0].title;
   }
   catch (error) {
   }
 }
 
 getData(URL);
+
+
+
 
 //cheap shark  & MapQuest to locate store
