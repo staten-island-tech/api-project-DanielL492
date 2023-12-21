3f638154eba78d4fdb421997490f4c2abc51ebbe
DanielL492 <144807703+DanielL492@users.noreply.github.com>
2023-12-21 09:10:17 -0500

    df

--- api-project/js/dom.js
+++ api-project/js/dom.js
@@ -2,6 +2,9 @@
 
 const DOMSelectors = {
     dropcontainer: document.getElementById("dropcontainer"),
+    top10btn: document.getElementById("Top10btn"),
+    container: document.getElementById("flexcontainer"),
+    storename: document.getElementById("storename")
 }
 
 export {DOMSelectors};
--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,27 +1,81 @@
 //const URL = `https://www.cheapshark.com/api/1.0/deals?&upperPrice=15`; 
 import { DOMSelectors } from "./dom";
 
-
-const URL = `https://www.cheapshark.com/api/1.0/stores?lastChange=deals?&pageSize=1&upperPrice=15`;
-async function getData(URL) {
-  try {
-    const response = await fetch(URL);
-    const data = await response.json();
-    console.log(data)
-    document.querySelector("h3").textContent = data[0].title;
+DOMSelectors.top10btn.addEventListener("click", function(){
+  const URL = `https://www.cheapshark.com/api/1.0/deals?&pageSize=10`;
+  async function getData(URL) {
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      console.log(data)
+      function insert(arr) {
+        arr.forEach((data) => DOMSelectors.container.insertAdjacentHTML("beforeend",
+        `<div class="flexcontainer">
+          <div class="card">
+            <h4 class="name">${data.title}</h1>
+            <img src="${data.thumb}" alt="Image" class="card-img"/> <br>
+            <h5 class="price"> Price: ${data.salePrice}</h5>
+              
+          </div>
+        </div>`
+        ))}
+      insert(data);
+    }
+    catch (error) {
+      console.log("error");
+      document.querySelector("h3").textContent = "Error";
+    }
   }
-  catch (error) {
-    console.log("error");
-    document.querySelector("h3").textContent = "Error";
-  }
-}
+  getData(URL);
+})
 
-getData(URL);
 
-function makedropdown(arr) {
+function makedropdown() {
   DOMSelectors.dropcontainer.insertAdjacentHTML("beforeend", 
-  ''
-  )}
+  `<div class="dropcontainer">
+    <label for=stores>Search for Deals in Specific Stores:</label>
+    <select name="goofyahname" id="stores">
+      <option value="0">None</option>
+      <option value="1">Steam</option>
+      <option value="2">GamersGate</option>
+      <option value="3">GreenManGaming</option>
+      <option value="4">Amazon</option>
+      <option value="5">Gamestop</option>
+      <option value="6">Direct2Drive</option>
+      <option value="7">GOG</option>
+      <option value="8">Origin</option>
+      <option value="9">Get Games</option>
+      <option value="10">Shiny Loot</option>
+      <option value="11">Humble Store</option>
+      <option value="12">Desura</option>
+      <option value="13">Uplay</option>
+      <option value="14">IndieGameStand</option>
+      <option value="15">Fanatical</option>
+      <option value="16">Gamesrocket</option>
+      <option value="17">Games Republic</option>
+      <option value="18">SilaGames</option>
+      <option value="19">Playfield</option>
+      <option value="20">ImperialGames</option>
+      <option value="21">WinGameStore</option>
+      <option value="22">FunStockDigital</option>
+      <option value="23">GameBillet</option>
+      <option value="24">Voidu</option>
+      <option value="25">Epic Games Store</option>
+      <option value="26">Razer Game Store</option>
+      <option value="27">Gamesplanet</option>
+      <option value="28">Gamesload</option>
+      <option value="29">2Game</option>
+      <option value="30">IndieGala</option>
+      <option value="31">Blizzard Shop</option>
+      <option value="32">AllYouPlay</option>
+      <option value="33">DLGamer</option>
+      <option value="34">Noctre</option>
+      <option value="35">DreamGame</option>
+  </select>
+</div>`
+)}
+
+makedropdown()
 
 
 
