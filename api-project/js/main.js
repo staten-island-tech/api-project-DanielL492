a9f3b65a07a809a95e06df1be299ba6f1f35890d
DanielL492 <144807703+DanielL492@users.noreply.github.com>
2024-01-03 09:12:18 -0500

    df

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -15,7 +15,6 @@ DOMSelectors.top10btn.addEventListener("click", function(){
             <h4 class="name">${data.title}</h1>
             <img src="${data.thumb}" alt="Image" class="card-img"/> <br>
             <h5 class="price"> Sale Price: ${data.salePrice}</h5>
-            <h6 id="${storeID}">Store: ${storeID}</h3>
           </div>
         </div>`
         ))}
@@ -25,8 +24,10 @@ DOMSelectors.top10btn.addEventListener("click", function(){
     catch (error) {
       console.log("error");
       document.querySelector("h3").textContent = "Error";
-    }
-  async function getStore(brlr) {
+    }}
+    getData(URL);
+  })
+  /*async function getStore(brlr) {
     try {
       const response = await fetch(brlr);
       const data = await response.json();
@@ -43,10 +44,8 @@ DOMSelectors.top10btn.addEventListener("click", function(){
     }
   }}
   let BRL = `https://www.cheapshark.com/api/1.0/stores`;
-  getStore(BRL);
-  console.log(BRL);
-  getData(URL);
-})
+  //getStore(BRL);
+  console.log(BRL);  <h6 id="${storeID}">Store: ${storeID}</h3>*/
 
 function clearcards() {
   const card = document.querySelectorAll(".card");
@@ -62,25 +61,15 @@ function makedropdown() {
       <option value="1">Steam</option>
       <option value="2">GamersGate</option>
       <option value="3">GreenManGaming</option>
-      <option value="4">Amazon</option>
-      <option value="5">Gamestop</option>
-      <option value="6">Direct2Drive</option>
       <option value="7">GOG</option>
       <option value="8">Origin</option>
-      <option value="9">Get Games</option>
       <option value="10">Shiny Loot</option>
       <option value="11">Humble Store</option>
       <option value="12">Desura</option>
       <option value="13">Uplay</option>
       <option value="14">IndieGameStand</option>
       <option value="15">Fanatical</option>
-      <option value="16">Gamesrocket</option>
-      <option value="17">Games Republic</option>
-      <option value="18">SilaGames</option>
-      <option value="19">Playfield</option>
-      <option value="20">ImperialGames</option>
       <option value="21">WinGameStore</option>
-      <option value="22">FunStockDigital</option>
       <option value="23">GameBillet</option>
       <option value="24">Voidu</option>
       <option value="25">Epic Games Store</option>
@@ -90,7 +79,6 @@ function makedropdown() {
       <option value="29">2Game</option>
       <option value="30">IndieGala</option>
       <option value="31">Blizzard Shop</option>
-      <option value="32">AllYouPlay</option>
       <option value="33">DLGamer</option>
       <option value="34">Noctre</option>
       <option value="35">DreamGame</option>
@@ -98,14 +86,39 @@ function makedropdown() {
 </div>`
 )}
 
-/*document.querySelector("#stores").addEventListener("change", function(){
-  let URL = "";
-  async function getStores() {
-
-  }
-})*/
-
 makedropdown()
+
+document.querySelector("#stores").addEventListener("change", function(){
+  let URL = `https://www.cheapshark.com/api/1.0/deals?storeID=${document.querySelector("#stores").value}`;
+  async function getStores(URL) {
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      console.log(data)
+      function insert(arr) {
+        arr.forEach((data) => DOMSelectors.container.insertAdjacentHTML("beforeend",
+        `<div class="flexcontainer">
+          <div class="card">
+            <h4 class="name">${data.title}</h1>
+            <img src="${data.thumb}" alt="Image" class="card-img"/> <br>
+            <h5 class="price"> Sale Price: ${data.salePrice}</h5>
+          </div>
+        </div>`
+        ))
+        if (arr.length == 0) {
+          document.querySelector("h5").textContent = "No Deals Found";
+        }
+      }
+      clearcards();
+      insert(data);
+    }
+    catch (error) {
+      console.log("error");
+      document.querySelector("h4").textContent = "Error";
+    }}
+    getStores(URL);
+})
+
 
 
 
